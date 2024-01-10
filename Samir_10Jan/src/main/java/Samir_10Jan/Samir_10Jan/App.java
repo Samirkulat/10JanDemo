00d557ddff4cc9a61d59e68709b8643fd75bd935
samirk4 <samir.kulat@gmail.com>
2024-01-10 09:22:30 +0530

    Simpple

---
+++ Samir_10Jan/src/main/java/Samir_10Jan/Samir_10Jan/App.java
@@ -0,0 +1,13 @@
+package Samir_10Jan.Samir_10Jan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
